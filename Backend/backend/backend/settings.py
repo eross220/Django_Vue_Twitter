508d9c965676d93be27c739280dad4d26e934003
Eross220 <blacktigerbusinesswork@gmail.com>
2023-03-09 08:04:38 +0100

    Site model and setting

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Files
+MEIDA_ROOT = os.path.joiin(BASE_DIR,'mediafiles')
+MEDIA_URL = '/media/'
