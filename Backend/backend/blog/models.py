d262ff7c8d5963f97fc8c8d4c11ef50ffb2d4da0
Eross220 <blacktigerbusinesswork@gmail.com>
2023-03-10 03:35:51 +0100

    add category, tag and post model

--- Backend/backend/blog/models.py
+++ Backend/backend/blog/models.py
@@ -30,3 +30,53 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = '3. Categories'
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = '4. Tags'
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    content = models.RichTextField()
+    featured_image = models.ImageField(upload_to='posts/featured_images/%Y/%m/%d/')
+    is_published = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+
+    likes = models.ManyToManyField(User, related_name='post_like')
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tag = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = '5. Posts'
+
+    def __str__(self):
+        return self.title
+
+    def get_number_of_likes(self):
+        return self.likes.count()
